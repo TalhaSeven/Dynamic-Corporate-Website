86c44051f6d2ecda55beee6d49887264c69014b1
ugurbiricik <biricik.ugurr@gmail.com>
2023-09-05 18:42:09 +0200

    home-style-2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Slider2 from './components/Slider2';
 
 function App() {
   return (
     <div>
-      <h1>Milestone</h1>
+    
+ <Slider2/>
+
+ 
     </div>
   );
 }
