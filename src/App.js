3ab8e9cf363d5c90e72cf6b1bc78273993625b27
ugurbiricik <biricik.ugurr@gmail.com>
2023-09-05 19:58:25 +0200

    Merge branch 'features/slider' of https://github.com/eurotechmilestone/milestone into features/slider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-
-import './App.css';
-import Slider2 from './components/Slider2';
+import "./App.css";
+import Slider2 from "./components/Slider2";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-  
-      <h1>Milestone Project</h1>
-      <Slider2/>
+      <Navbar />
+      <Slider2 />
     </div>
   );
 }
