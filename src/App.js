161b30f61b2caa21b81649c4a2e09c34e037c7e8
Talha Seven <131260032+TalhaSeven@users.noreply.github.com>
2023-09-06 17:17:03 +0300

    Faq Add

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import Slider2 from "./components/Slider2";
 import Navbar from "./components/Navbar";
 import { BrowserRouter } from "react-router-dom";
+import Faq from "./components/Faq";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Slider2 />
-    
+      {/* <Faq/> */}
     </BrowserRouter>
   );
 }
