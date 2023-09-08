28d110ce9637a898809939a7aafa159c02815b50
Suat Ceran <slsmr48@gmail.com>
2023-09-08 17:48:40 +0200

    wayToLearn1

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Faq from "./components/Faq";
 import ContactForm from "./components/ContactForm";
 
 import Table from "./components/table/Table";
+import WayToLearn from "./components/WayToLearn";
 
 
 function App() {
@@ -27,6 +28,7 @@ function App() {
        <Faq/>
       <SwiperOur />
       <Settling />
+      <WayToLearn/>
       <Table/>
 
     </BrowserRouter>
