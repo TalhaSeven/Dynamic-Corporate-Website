fea33857531a3d336052a09254aaf79b4b2754a1
ugurbiricik <biricik.ugurr@gmail.com>
2023-09-15 15:13:59 +0200

    ugurs project

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import "./App.css";
 import "swiper/css";
 import Navbar from "./components/navbar/Navbar";
 import { BrowserRouter } from "react-router-dom";
-
-<<<<<<< HEAD
 // import OurTeachers from "./components/OurTeachers";
-=======
 // import Gallery from "./components/Gallery/Gallery";
->>>>>>> main
+
 // import Slider2 from "./components/Slider2";
 // import SwiperOur from "./components/SwiperOur";
 // import Settling from "./components/Settling";
-import WayToLearn from "./components/WayToLearn";
+// import WayToLearn from "./components/WayToLearn";
 import Footer from "./components/Footer";
 import Footer2 from "./components/Footer2";
 // import Faq from "./components/Faq";
@@ -21,7 +18,7 @@ import Footer2 from "./components/Footer2";
 // import ProgramDetails from "./components/ProgramDetails";
 // import Programs from "./components/programs/Programs";
 // import BreadCrumb from "./components/BreadCrumb";
-import Parentsays from "./components/Parentsays";
+// import Parentsays from "./components/Parentsays";
 
 function App() {
   return (
@@ -29,8 +26,8 @@ function App() {
       <Navbar />
       <h1>Buraya bir şey eklemeyin arkadaşlar</h1>
 
-           <Footer/>
-           <Footer2/>
+      <Footer />
+      <Footer2 />
       {/* <Slider2 />
       <ContactForm />
       <Faq />
@@ -47,7 +44,6 @@ function App() {
       <BestFor />
       <Gallery/>
       <BreadCrumb /> */}
-
     </BrowserRouter>
   );
 }
