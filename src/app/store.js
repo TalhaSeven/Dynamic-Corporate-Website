6df8e3abb19812189572ad3e233e9365bbeb4463
denizderK <denizderya8668@gmail.com>
2023-09-16 21:17:45 +0300

    redux-thunk

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import homeReducer from '../features/homeSlice'
-import menuReducer from '../features/menuSlice'
+import menuReducer from "../features/menuSlice"
 import languageReducer from '../features/languageSlice'
 
-export const store = configureStore({
+ const store = configureStore({
   reducer: {
     home:homeReducer,
     menu:menuReducer,
     language:languageReducer,
   },
 })
+export default store;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,9 +7,10 @@ import {
   } from "tw-elements";
 import NavbarTop from './NavbarTop';
 import MenuNavbar from './MenuNavbar';
-import axios from 'axios';
+// import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
-import { setMenu } from '../../features/menuSlice';
+//  import  {setMenu}  from '../../features/menuSlice';
+import { getMenu } from '../../features/menuSlice';
 
   initTE({ Collapse, Dropdown });
 
@@ -23,19 +24,20 @@ const {menu} =useSelector((state) => state.menu)
 console.log(menu)
 
 
-const menuFunction =async()=>{
-  const url="https://fuatmercan.com/kids/api/v1/menu.php?lang=en&token=frpQ8/CDUfTsNoUUkbL0121PkOOjWJ1eDOfkQd3lWz3n/ZY/zu28pvFTW34u7M8CTKAdaQeWkT42n1rMsw==588cb53f476e2e13cde27315433d124c"
-  try {
-   const {data} =await axios(url)
-   dispatch(setMenu(data))
-  //  setMenu(data)
-   console.log(data)
-  } catch (error) {
-    console.log(error)  
-  }
-}
+// const menuFunction =async()=>{
+//   const url="https://fuatmercan.com/kids/api/v1/menu.php?lang=en&token=frpQ8/CDUfTsNoUUkbL0121PkOOjWJ1eDOfkQd3lWz3n/ZY/zu28pvFTW34u7M8CTKAdaQeWkT42n1rMsw==588cb53f476e2e13cde27315433d124c"
+//   try {
+//    const {data} =await axios(url)
+//    dispatch(setMenu(data))
+//   //  setMenu(data)
+//    console.log(data)
+//   } catch (error) {
+//     console.log(error)  
+//   }
+// }
 useEffect(() => {
-  menuFunction()
+  // menuFunction()
+ dispatch(getMenu())
 }, [])
 
 
@@ -95,7 +97,7 @@ useEffect(() => {
           className="list-style-none mr-auto flex flex-col pl-0 lg:flex-row"
           data-te-navbar-nav-ref=""
         >
-       {menu.map((item, index) => <MenuNavbar item={item} key={index}/> )}
+       {menu?.map((item, index) => <MenuNavbar item={item} key={index}/> )}
        
 
 
